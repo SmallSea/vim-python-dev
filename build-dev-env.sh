66df6b93662fdfee09d45bf07fb73100562e3b07
SmallSea <smallsea@gmail.com>
2018-09-26 23:01:07 +0800

    Support python development in MacOS.

--- build-dev-env.sh
+++ build-dev-env.sh
@@ -1,13 +1,33 @@
-#!/bin/sh
+#!/bin/bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ ${machine} = "UNKNOWN:${unameOut}" ]; then
+    echo "This script only supported Ubuntu/Debuan and MacOS."
+    echo "Your OS is ${machine} and not in suppot list."
+    exit 127
+fi
+
+echo "Your platform is: ${machine}, starting to install:"
 
 cp .screenrc ~
 cp .vimrc ~
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qal
-sudo apt-update
-sudo apt-get -y install build-essential cmake
-sudo apt-get -y install python-dev python3-dev
+
+if [ ${machine} = "Linux"]; then
+    sudo apt-update
+    sudo apt-get -y install build-essential cmake
+    sudo apt-get -y install python-dev python3-dev
+elif [ ${machine} = "Mac" ]; then
+    brew install python3 cmake
+fi
 
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py
